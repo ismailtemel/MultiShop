c55ba0bb2ab8e15808205550eff1e85795f287a9
ismailtemel <64991440+ismailtemel@users.noreply.github.com>
2025-05-07 17:48:50 +0300

    New Update

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Category")]
+    public class CategoryController : Controller
+    {
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            CategoryViewBagList();
+            return View();
+        }
+
+        void CategoryViewBagList()
+        {
+            ViewBag.v1 = "Anasayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+            ViewBag.v0 = "Kategori İşlemleri";
+        }
+    }
+}
--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -20,10 +20,20 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
+
+
 
 
 app.Run();
