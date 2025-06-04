7e6f962a1662bb79055732ab21bf491ebbbc73dc
ismailtemel <64991440+ismailtemel@users.noreply.github.com>
2025-06-04 17:34:40 +0300

    New Update

---
+++ Frontends/MultiShop.DtoLayer/CargoDtos/CargoCustomerDtos/GetCargoCustomerByIDDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.DtoLayer.CargoDtos.CargoCustomerDtos
+{
+    public class GetCargoCustomerByIDDto
+    {
+        public int cargoCustomerID { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string district { get; set; }
+        public string city { get; set; }
+        public string address { get; set; }
+        public string userCustomerID { get; set; }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/CargoServices/CargoCustomerServices/CargoCustomerService.cs
@@ -0,0 +1,21 @@
+﻿using MultiShop.DtoLayer.CargoDtos.CargoCustomerDtos;
+
+namespace MultiShop.WebUI.Services.CargoServices.CargoCustomerServices
+{
+    public class CargoCustomerService : ICargoCustomerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CargoCustomerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<GetCargoCustomerByIDDto> GetByIDCargoCustomerInfoAsync(string id)
+        {
+            var responseMessage = await _httpClient.GetAsync("CargoCustomers/GetCargoCustomerByID?id=" + id);
+            var values = await responseMessage.Content.ReadFromJsonAsync<GetCargoCustomerByIDDto>();
+            return values;
+        }
+    }
+}
